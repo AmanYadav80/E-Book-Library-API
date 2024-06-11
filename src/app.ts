f90b6f43557a8eafd1bc94e80e1bb1133a1c121d
AmanYadav80 <amanydv049@gmail.com>
2024-06-11 23:44:50 +0530

    create user route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app=express();
 
@@ -9,6 +10,7 @@ app.get('/', (req,res,next)=>{
     res.json({message:"Welcome to ebook api's"});
 });
 
+app.use('/api/users',userRouter);
 
 //global error handler
 app.use(globalErrorHandler);
---
+++ src/user/userController.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express"
+
+const createUser = async (req: Request,res: Response,next: NextFunction) => {
+    res.status(200).json({
+        message:"User created",
+    });
+};
+
+export { createUser };
