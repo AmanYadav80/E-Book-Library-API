ef229437072dfcbbb23315477aec3ffeb4906492
AmanYadav80 <amanydv049@gmail.com>
2024-06-13 22:24:56 +0530

    add book model

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 const app=express();
 
@@ -13,6 +14,8 @@ app.get('/', (req,res,next)=>{
 });
 
 app.use('/api/users',userRouter);
+
+app.use('/api/books',bookRouter);
 
 //global error handler
 app.use(globalErrorHandler);
